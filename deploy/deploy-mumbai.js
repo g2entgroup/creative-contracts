cdfae45951d240c331387cc556cf87c5a5d15d50
CrispyMangoes <crispymangoes@gmail.com>
2021-05-28 13:41:19 -0700

    Make poolOwner public

--- deploy/deploy-mumbai.js
+++ deploy/deploy-mumbai.js
@@ -2,6 +2,7 @@ const LINK_TOKEN_ADDRESS = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB";
 module.exports = async ({getNamedAccounts, deployments}) => {
     const {deploy} = deployments;
     const {deployer} = await getNamedAccounts();
+    /*
     const twitterVerify = await deploy('twitterverify', {
         from: deployer,
         args: [],
@@ -22,5 +23,11 @@ module.exports = async ({getNamedAccounts, deployments}) => {
     const rng = await RNG.attach(rngContract.address);
     await rng.transferOwnership(poolFactory.address, {gasLimit: 500000});
     console.log("New RNG Owner: ", await rng.owner());
+    */
+    const PoolContract = await deploy('Pool', {
+      from: deployer,
+      args: ["test", "test", "100", "0xc59Cd7D4A979373253476dAe531333eE45aEF2f4", "0xc59Cd7D4A979373253476dAe531333eE45aEF2f4", "0xa5E5860B34ac0C55884F2D0E9576d545e1c7Dfd4", "0xc59Cd7D4A979373253476dAe531333eE45aEF2f4", "300", "100", "100", "100"],
+      log: true,
+    });
   };
   module.exports.tags = ['PoolFactory'];
