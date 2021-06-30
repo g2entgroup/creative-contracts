2dcbbbc69a28f6e55fc0864a38623379d2458af3
G2 <bracy.gawain2014@gmail.com>
2021-06-30 10:46:27 -0400

    ERC721
    Added ERC721 Contract.

--- contracts/CreativeNFT.sol
+++ contracts/CreativeNFT.sol
@@ -1,30 +1,46 @@
-pragma solidity 0.7.5;
+pragma solidity ^0.8.0;
 //SPDX-License-Identifier: MIT
 
 //import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+//import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 //learn more: https://docs.openzeppelin.com/contracts/3.x/erc721
 
 // GET LISTED ON OPENSEA: https://testnets.opensea.io/get-listed/step-two
 
-contract CreativeNFT is ERC721 {
+contract CreativeNFT is ERC721URIStorage, Ownable {
+
+   string baseURI;
 
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  constructor() public ERC721("CreativeNFT", "CNFT") {
-    _setBaseURI("https://ipfs.io/ipfs/");
+  // function setBaseURI(string memory baseURI_) external {
+  //       baseURI = baseURI_;
+  //   }
+
+  //   function _baseURI() internal view override returns (string memory) {
+  //       return baseURI;
+  //   }
+
+  constructor() public ERC721("Creative NFT", "CNFT") {
+    // setBaseURI("https://ipfs.io/ipfs/");
   }
 
-  function mintItem(address to, string memory tokenURI)
+  function mintItem( 
+    address account,
+    string memory tokenURI
+    )
       public
+      onlyOwner
       returns (uint256)
   {
       _tokenIds.increment();
 
       uint256 id = _tokenIds.current();
-      _mint(to, id);
+      _mint(account, id);
       _setTokenURI(id, tokenURI);
 
       return id;
