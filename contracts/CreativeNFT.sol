d8b69b7b73d1aa64bc2dc93b957265facf9abbaa
G2 <bracy.gawain2014@gmail.com>
2021-08-22 15:26:04 -0400

    Updated OpenZepplin

--- contracts/CreativeNFT.sol
+++ contracts/CreativeNFT.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzepplin/contracts/token/ERC721/ERC721.sol";
-import "@openzepplin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzepplin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzepplin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
-import "@openzepplin/contracts/access/AccessControlEnumerable.sol";
-import "@openzepplin/contracts/utils/Context.sol";
-import "@openzepplin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 /**
  * @dev {ERC721} token, including:
